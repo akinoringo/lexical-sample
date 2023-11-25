8b16ba14962da0bc1da484d074c803b4a57fbe62
akinoringo <wbraver@outlook.jp>
2023-11-25 22:00:19 +0900

    introduce lexical and make sample editor

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import './App.css';
+import MyEditor from './components/MyEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MyEditor</h1>
+      <MyEditor />
+    </div>
+  );
+}
+
+export default App;
